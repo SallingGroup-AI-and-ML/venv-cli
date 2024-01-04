a8a8a52c6d29668d846c3c9e75913e877729e50d
Stefan Mejlgaard <stefan.hasselgren.mejlgaard@sallinggroup.com>
2024-01-04 21:25:13 +0100

    Fix #27: Running uninstall.sh does not remove comment line from rc files

--- src/venv-cli/uninstall.sh
+++ src/venv-cli/uninstall.sh
@@ -9,13 +9,35 @@ post() {
   sudo rm -r "${_venv_dir}" || true
 }
 
+backup_rcfile() {
+  local rcfile="$1"
+
+  echo -e "# This backup file was created by venv-cli during uninstall" > "${rcfile}.old"
+  cat "${rcfile}" >> "${rcfile}.old"
+  echo "Created backup of ${rcfile} at ${rcfile}.old"
+}
+
+remove_source_lines() {
+  # Remove the 'source' lines and the comment above it
+  local source_lines_to_remove=$(("$1" - 1))
+  local rcfile="$2"
+
+  # Remove a specific number of lines from the rc file.
+  # The result is then redirected to a .tmp rc file, which is then moved to the original rc file.
+  local delete_pattern=",+${source_lines_to_remove}d"
+  sed "\|\# Source function script for 'venv' command|${delete_pattern}" < "${rcfile}" > "${rcfile}.tmp"
+  mv "${rcfile}.tmp" "${rcfile}"
+}
+
 uninstall_common() {
   local rcfile="$1"
   local completion_target="$2"
+  local lines_to_remove="$3"
 
-  # Remove the line from shell config that sources the script
-	sed -i "\|.*Source autocompletions for 'venv' command|d" "${rcfile}"
-	sed -i "\|\. ${_venv_dir}/venv|d" "${rcfile}"
+  if command grep -q "${_venv_dir}/venv" "${rcfile}"; then
+    backup_rcfile "${rcfile}"
+    remove_source_lines "${lines_to_remove}" "${rcfile}"
+  fi
 
   if [ -f "${completion_target}" ]; then
     sudo rm "${completion_target}"
@@ -25,17 +47,21 @@ uninstall_common() {
 uninstall_bash() {
   local rcfile="${HOME}/.bashrc"
   local completion_target="/usr/share/bash-completion/completions/venv"
+  local rcfile_source_lines=2
 
-  uninstall_common "${rcfile}" "${completion_target}"
+  uninstall_common "${rcfile}" "${completion_target}" "${rcfile_source_lines}"
+
+  echo "venv command and completions removed from bash"
 }
 
 uninstall_zsh() {
   local rcfile="${HOME}/.zshrc"
   local completion_target="/usr/local/share/zsh/site-functions/_venv"
+  local rcfile_source_lines=4
 
-  uninstall_common "${rcfile}" "${completion_target}"
-  sed -i "\|fpath+=( ${_install_dir} )|d" "${rcfile}"
-  sed -i "\|autoload -Uz venv|d" "${rcfile}"
+  uninstall_common "${rcfile}" "${completion_target}" "${rcfile_source_lines}"
+
+  echo "venv command and completions removed from zsh"
 }
 
 main() {
